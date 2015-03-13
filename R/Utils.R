1babfd52035ec74c579c86a95e27159f0fcb419a
Qi Dai <daiqi5477@gmail.com>
2015-03-13 15:08:25 -0400

    str compare, match(), subst()

--- R/Utils.R
+++ R/Utils.R
@@ -5,3 +5,7 @@ fileList <- dir("../path/")
 smpl$LOAD_DT <- format(as.Date(smpl$LOAD_DT), "%d-%b-%y")
 
 smpl <- merge(binRst, clms, by="KEY")
+
+str1==wtr2 #works
+match(str1, str2) # works
+substr(str1, start, end)
