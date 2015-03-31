a615142f89ac8707851d953a9591c5e130620c92
Qi Dai <daiqi5477@gmail.com>
2015-03-31 09:33:46 -0400

    Update Utils.R
    get unique values of two factors
    
    get common items of two factors

--- R/Utils.R
+++ R/Utils.R
@@ -9,3 +9,12 @@ smpl <- merge(binRst, clms, by="KEY")
 str1==wtr2 #works
 match(str1, str2) # works
 substr(str1, start, end)
+
+# get unique values of two factors
+ECB_List <- unique(df$ECB_VFG)
+PRED_List <- unique(df$Pred_VFG)
+VFG_CD <- unique(unlist(list(ECB_List, PRED_List)))
+
+# get common items of two factors
+intersect(intersect(a,b),c)
+Reduce(intersect,list(a,b,c))
