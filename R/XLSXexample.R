04205634befe723a72fe35a3b531297e4e1ea2e2
Qi Dai <daiqi5477@gmail.com>
2015-03-31 10:04:27 -0400

    read, write

--- R/XLSXexample.R
+++ R/XLSXexample.R
@@ -1,5 +1,13 @@
 options(java.parameters = "-Xmx16g" ) 
 
 library(XLConnect) 
+
+# read sheet
 wb <- loadWorkbook("F3.xlsx") 
 df <- readWorksheet(wb, sheet="Sheet1", header = TRUE)
+
+# write sheet
+wbo <- loadWorkbook("F3_out.xlsx", create = TRUE)
+createSheet(wbo, name = "F3")
+writeWorksheet(wbo, rst, sheet="F3")
+saveWorkbook(wbo)
